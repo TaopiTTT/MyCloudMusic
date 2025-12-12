c100f8548725b122027700fecdbc7f57575808d8
Tao <2063108163@qq.com>
2025-12-12 17:04:29 +0800

    project init

---
+++ MyCloudMusic/MyCloudMusic/MyCloudMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCloudMusicApp.swift
+//  MyCloudMusic
+//
+//  Created by Tao on 2025/12/12.
+//
+
+import SwiftUI
+
+@main
+struct MyCloudMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
