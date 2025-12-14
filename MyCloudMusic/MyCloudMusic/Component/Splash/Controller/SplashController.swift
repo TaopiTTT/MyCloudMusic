8631354674d1b200ef6fdd0eb45a328ad5504066
Tao <2063108163@qq.com>
2025-12-14 14:27:14 +0800

    统一颜色管理

--- MyCloudMusic/MyCloudMusic/Component/Splash/Controller/SplashController.swift
+++ MyCloudMusic/MyCloudMusic/Component/Splash/Controller/SplashController.swift
@@ -12,7 +12,7 @@ class SplashController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .colorBackground
         
         // 创建一个相对容器
         let container = TGRelativeLayout()
